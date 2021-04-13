5ff2bc7c78077e516b54651e79a222d1da2bd8bc
Pio Mendoza <pio.lauren.mendoza@gmail.com>
2021-04-13 13:44:54 +0800

    completed 7_completing_a_program

---
+++ part1_the_basics/6_writing_a_program/main.cpp
@@ -0,0 +1,8 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+    // int a;
+    // cin >> a;
+    cout << bool(cin);
+}
---
+++ part1_the_basics/7_completing_a_program/main.cpp
@@ -0,0 +1,11 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+    while (cin)
+    {
+        int val;
+        cin >> val;
+    }
+    return 0;
+}
