1a8d66929b5b75bff5d5baee21923a2bff637e20
Pio Mendoza <pio.lauren.mendoza@gmail.com>
2021-04-29 14:17:36 +0800

    completed 10_input_and_output_streams

--- part2_input_and_output_streams/10_input_and_output_streams/main.cpp
+++ part2_input_and_output_streams/10_input_and_output_streams/main.cpp
@@ -1,18 +1,45 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <stdio.h>
 
 int main()
 {
-    std::cout << "Input String name: " << std::endl;
-    std::string file_name{};
-    std::cin >> file_name;
-    // defining an input file stream
-    std::ifstream ist{file_name};
-    // no input file stream was opened
-    if (!ist)
-        throw std::runtime_error("hahaha");
-    while (true)
-    {
-    }
+    // std::cout << "Input String name: " << std::endl;
+    // std::string file_name{};
+    // std::cin >> file_name;
+    // // defining an input file stream
+    // std::ifstream ist{file_name};
+    // // no input file stream was opened
+    // if (!ist)
+    //     throw std::runtime_error("hahaha");
+    // while (true)
+    // {
+    // }
+    // int n{};
+    // std::cin >> n;
+    // std::exception();
+    // if (std::cin.fail())
+    // {
+    //     std::cin.clear();
+    //     char c;
+    //     std::cin >> c;
+    //     std::cout << "The value inserted is " << c << std::endl;
+    // }
+    // std::cout << "The value of n is " << n << std::endl;
+    // enum day
+    // {
+    //     araw,
+    //     gabi
+    // };
+    // enum class ano
+    // {
+    //     gaga,
+    //     gago
+    // };
+    // std::cout << static_cast<bool>(day::gabi == ano::gago) << std::endl;
+    char array[3];
+    printf("%p", array);
+    std::cout << std::endl;
+    printf("%p", &array);
 }
